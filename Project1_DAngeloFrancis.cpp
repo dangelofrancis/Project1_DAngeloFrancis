80c5259d640a47f7494623b78b328e8d96f49bc4
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-02-10 21:20:18 -0500

    Added comments

--- Project1_DAngeloFrancis.cpp
+++ Project1_DAngeloFrancis.cpp
@@ -11,14 +11,78 @@
 #include <random>
 using namespace std;
 
+//************************************************
+// Description of the function displayTitle      *
+// This function displays the title of the Rock  *
+// Paper Scissors game                           *
+//************************************************
+
 void displayTitle();
+
+//************************************************
+// Description of the function getPlayerChoice   *
+// This function asks the user to select Rock,   *
+// Paper, Scissors, or Quit by entering a number *
+//************************************************
+
 int getPlayerChoice(bool& gameRunning);
+
+//************************************************
+// Description of the function getComputerChoice *
+// This function randomly generates a number 1-3 *
+// Each of the numbers corresponds to Rock, Paper*
+// or Scissors, and whichever is generated will  *
+// serve as the computer's choice                *
+//************************************************
+
 int getComputerChoice();
+
+//************************************************
+// Description of the function showComputerChoice*
+// This function takes the randomly generated    *
+// number from getComputerChoice, determines     *
+// whether it will be Rock, Paper, Or Scissors,  *
+// and displays the computer's choice on screen  *
+//************************************************
+
 void showComputerChoice(int computerChoice);
+
+//************************************************
+// Description of the function determineWinner   *
+// This function compares the player's choice    *
+// with the computer's choice, and figures out   *
+// which one will win                            *
+//************************************************
+
 string determineWinner(int playerChoice, int computerChoice);
+
+//************************************************
+// Description of the function displayWinner     *
+// This function displays a short message of who *
+// won the round                                 *
+//************************************************
+
 void displayWinner(string winner);
+
+//************************************************
+// Description of the function updateScores      *
+// This function updates the scoreboard if ther  *
+// was a winner for the round                    *
+//************************************************
+
 void updateScores(string winner, int scores[]);
+
+//************************************************
+// Description of the function displayScores     *
+// This fucntion displays a scoreboard after the *
+// round ends                                    *
+//************************************************
+
 void displayScores(int scores[]);
+
+//************************************************
+// The main function                             *
+//************************************************
 
 int main() {
     bool gameRunning = true;
