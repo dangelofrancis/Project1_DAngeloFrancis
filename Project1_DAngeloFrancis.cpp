e99aaddfb3db0662a8704aa707b845c406a2f7f2
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-02-10 23:42:26 -0500

    Added stuff

--- Project1_DAngeloFrancis.cpp
+++ Project1_DAngeloFrancis.cpp
@@ -1,20 +1,106 @@
-// Project1_DAngeloFrancis.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//************************************************
+// TITLE: Grade Book                *
+// FILE NAME: Project1_DAngeloFrancis.cpp        *
+// PROGRAMMER: D'Angelo Francis                  *
+// DATE: February 10, 2025                       *
+// REQUIREMENTS:Read the student’s names and test*
+// scores from a file, calculate the average of  *
+// the test scores and then provide a letter     *
+// grade for the average                         *
+//************************************************
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int MAX_TESTS = 10;
+const int MAX_STUDENTS = 60;
+
+string getFileName();
+ifstream openFile(string fileName);
+int readFile(ifstream& inputFile, string studentNames[], double testScores[][MAX_TESTS], int& numTests);
+void closeFile(ifstream& inputFile);
+void calculateAverages(double testScores[][MAX_TESTS], double averages[], int numStudents, int numTests);
+char getLetterGrade(double average);
+void displayGrades(string studentNames[], double averages[], int numStudents);
+
+int main() {
+    string studentNames[MAX_STUDENTS]; 
+    double testScores[MAX_STUDENTS][MAX_TESTS];
+    double averages[MAX_STUDENTS]; 
+    int numTests, numStudents;
+
+    string fileName = getFileName();
+    ifstream inputFile = openFile(fileName);
+
+    numStudents = readFile(inputFile, studentNames, testScores, numTests);
+    closeFile(inputFile);
+
+    calculateAverages(testScores, averages, numStudents, numTests);
+    displayGrades(studentNames, averages, numStudents);
+
+    return 0;
+}
+string getFileName() {
+    string fileName;
+    cout << "Enter the file name you wish to read: ";
+    cin >> fileName;
+    return fileName;
+}
+ifstream openFile(string fileName) {
+    ifstream inputFile;
+    inputFile.open(fileName);
+
+    if (!inputFile) {
+        cout << "*ERROR* Unable to open file." << endl;
+    }
+    cout << "*SUCCESS* File was opened.\n\n";
+    return inputFile;
+}
+int readFile(ifstream& inputFile, string studentNames[], double testScores[][MAX_TESTS], int& numTests) {
+    int numStudents = 0;  
+    numTests = 0;  
+
+    while (inputFile >> studentNames[numStudents]) { 
+        int count = 0;
+        while (count < MAX_TESTS && inputFile >> testScores[numStudents][count]) { 
+            count++;
+        }
+        if (numStudents == 0) {
+            numTests = count;  
+        }
+        numStudents++;
+    }
+    return numStudents; 
+}
+void closeFile(ifstream& inputFile) {
+    inputFile.close();
+}
+void calculateAverages(double testScores[][MAX_TESTS], double averages[], int numStudents, int numTests) {
+    for (int i = 0; i < numStudents; i++) {
+        double total = 0;
+        for (int j = 0; j < numTests; j++) {
+            total += testScores[i][j];
+        }
+        averages[i] = total / numTests;
+    }
+}
+char getLetterGrade(double average) {
+    if (average >= 90) return 'A';
+    else if (average >= 80) return 'B';
+    else if (average >= 70) return 'C';
+    else if (average >= 60) return 'D';
+    else return 'F';
+}
+void displayGrades(string studentNames[], double averages[], int numStudents) {
+    cout << left << setw(15) << "Student Name" << setw(10) << "Average" << "Grade" << endl;
+    cout << "______________________________\n";
+
+    for (int i = 0; i < numStudents; i++) {
+        cout << left << setw(15) << studentNames[i]
+            << setw(10) << fixed << setprecision(2) << averages[i]
+            << getLetterGrade(averages[i]) << endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
